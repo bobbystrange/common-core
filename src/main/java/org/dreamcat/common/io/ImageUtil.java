4ae7967bac5317139f84de9c524367ad36be90f4
tuke <tukeof@gmail.com>
2020-02-26 13:48:35 +0800

    refactor image code util

--- src/main/java/org/dreamcat/common/io/ImageUtil.java
+++ src/main/java/org/dreamcat/common/io/ImageUtil.java
@@ -1,7 +1,5 @@
 package org.dreamcat.common.io;
 
-import org.dreamcat.common.core.Pair;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -19,18 +17,22 @@ import java.util.Random;
 public class ImageUtil {
 
     // <image src="data:image/png,xxx">
-    public static String imageToBase64Source(RenderedImage image, String imageType) {
-        return String.format("data:image/%s;base64,%s", imageType, imageToBase64(image, imageType));
+    public static String base64ImageSource(RenderedImage image) {
+        return base64ImageSource(image, "png");
+    }
+
+    public static String base64ImageSource(RenderedImage image, String imageType) {
+        return String.format("data:image/%s;base64,%s", imageType, base64(image, imageType));
     }
 
     /**
      * image to base64
      *
      * @param image     image object
-     * @param imageType jpg, png
+     * @param imageType jpeg, png
      * @return base64
      */
-    public static String imageToBase64(RenderedImage image, String imageType) {
+    public static String base64(RenderedImage image, String imageType) {
         try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
             ImageIO.write(image, imageType, Base64.getEncoder().wrap(os));
             return os.toString(StandardCharsets.ISO_8859_1.name());
@@ -39,7 +41,47 @@ public class ImageUtil {
         }
     }
 
-    private static Color generateRandomColor(int rgbStart, int rgbEnd) {
+    // <image src="data:image/png,xxx">
+    public static String generateBase64ImageSource(String code, int width, int height) {
+        BufferedImage image = generateImage(code, width, height);
+        return base64ImageSource(image);
+    }
+
+    public static BufferedImage generateImage(String code, int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = null;
+        try {
+            g = image.getGraphics();
+            Random random = new Random();
+
+            g.setColor(randomColor(192, 256));
+            g.fillRect(0, 0, width, height);
+            g.setFont(new Font("DejaVu Sans", Font.ITALIC, 20));
+            g.setColor(randomColor(128, 192));
+            for (int i = 0; i < 155; i++) {
+                int x = random.nextInt(width);
+                int y = random.nextInt(height);
+                int xl = random.nextInt(12);
+                int yl = random.nextInt(12);
+                g.drawLine(x, y, x + xl, y + yl);
+            }
+
+            int length = code.length();
+            for (int i = 0; i < length; i++) {
+                String chr = String.valueOf(code.charAt(i));
+                g.setColor(randomColor(24, 136));
+                g.drawString(chr, 13 * i + 6, 16);
+            }
+        } finally {
+            if (g != null) g.dispose();
+            ;
+        }
+        return image;
+    }
+
+    // ==== ==== ==== ====    ==== ==== ==== ====    ==== ==== ==== ====
+
+    private static Color randomColor(int rgbStart, int rgbEnd) {
         Random random = new Random();
         if (rgbStart > 255) {
             rgbStart %= 256;
@@ -53,39 +95,4 @@ public class ImageUtil {
         int b = rgbStart + random.nextInt(bound);
         return new Color(r, g, b);
     }
-
-    public Pair<BufferedImage, String> generateImageCode(int width, int height, int length) {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        StringBuilder code = new StringBuilder(length);
-        Graphics g = null;
-        try {
-            g = image.getGraphics();
-            Random random = new Random();
-
-            g.setColor(generateRandomColor(192, 256));
-            g.fillRect(0, 0, width, height);
-            g.setFont(new Font("DejaVu Sans", Font.ITALIC, 20));
-            g.setColor(generateRandomColor(128, 192));
-            for (int i = 0; i < 155; i++) {
-                int x = random.nextInt(width);
-                int y = random.nextInt(height);
-                int xl = random.nextInt(12);
-                int yl = random.nextInt(12);
-                g.drawLine(x, y, x + xl, y + yl);
-            }
-
-            for (int i = 0; i < length; i++) {
-                String rand = String.valueOf(random.nextInt(10));
-                code.append(rand);
-                g.setColor(generateRandomColor(24, 136));
-                g.drawString(rand, 13 * i + 6, 16);
-            }
-        } finally {
-            if (g != null) g.dispose();
-            ;
-        }
-
-        return new Pair<>(image, code.toString());
-    }
-
 }
