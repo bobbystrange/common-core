759e7c99601c5a7e36f29e66b7c0fb077c94acd0
tuke <tukeof@gmail.com>
2020-03-08 21:39:05 +0800

    feature: CryptoUtil add hs512 & hmd5

--- src/main/java/org/dreamcat/common/crypto/CryptoUtil.java
+++ src/main/java/org/dreamcat/common/crypto/CryptoUtil.java
@@ -33,6 +33,18 @@ public class CryptoUtil {
         }
     }
 
+    public static String digestHMACMD5(String data, String key) {
+        return digestHMACMD5(data.getBytes(), key);
+    }
+
+    public static String digestHMACMD5(byte[] data, String key) {
+        try {
+            return HmacEnum.HMAC_MD5.digestToBase64(data, key.getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static String digestHMACSHA256(String data, String key) {
         return digestHMACSHA256(data.getBytes(), key);
     }
@@ -40,6 +52,18 @@ public class CryptoUtil {
     public static String digestHMACSHA256(byte[] data, String key) {
         try {
             return HmacEnum.HMAC_SHA_256.digestToBase64(data, key.getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String digestHMACSHA512(String data, String key) {
+        return digestHMACSHA512(data.getBytes(), key);
+    }
+
+    public static String digestHMACSHA512(byte[] data, String key) {
+        try {
+            return HmacEnum.HMAC_SHA_512.digestToBase64(data, key.getBytes());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
